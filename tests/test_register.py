ab9340cd581e4a37ad42d52a394ae7266a48d465
gtamie <giulianatamie@gmail.com>
2026-05-15 10:18:20 -0300

    The function test_register_with_valid_data was created and validated.

--- tests/test_register.py
+++ tests/test_register.py
@@ -8,9 +8,7 @@ class TestRegister:
         print(f"\nURL atual: {driver.current_url}")
         assert "register" in driver.current_url.lower()
 
-    '''
-    OBS.: No teste abaixo, quando usuário já existe, o teste está falhando com TimeoutException ao invés de falhar por
-    não ter encontrado a mensagem de sucesso. Vou manter esse teste comentado até conseguir corrigir.
+
     def test_register_with_valid_data(self, register_page):
         register_page.fill_first_name("John")
         register_page.fill_last_name("Doe")
@@ -20,10 +18,9 @@ class TestRegister:
         register_page.fill_zip_code("10001")
         register_page.fill_phone("1234567890")
         register_page.fill_ssn("123-45-6789")
-        register_page.fill_username("test")
+        register_page.fill_username("test01")
         register_page.fill_password("Test@1234")
         register_page.confirm_password("Test@1234")
         register_page.click_register_button()
 
         assert "Your account was created successfully" in register_page.get_success_message()
-'''
